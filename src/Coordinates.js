44a190c7ae2336c9bc1a189f2c4e9740bbee2398
Kentywey <kweymeesch@gmail.com>
2017-09-29 15:26:07 +0200

    Test succés pour coordonne A1

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,21 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+
+    this.is_valid = function () {
+
+        var tableauJeux = [];
+        tableauJeux[0] = ["C"];
+        tableauJeux[1] = ["B", "C", "D", "E"];
+        tableauJeux[2] = ["A", "B", "C", "D", "E", "F", "G"];
+        tableauJeux[3] = ["B", "C", "D", "E", "F", "G"];
+        tableauJeux[4] = ["B", "C", "D", "E", "F", "G", "H"];
+        tableauJeux[5] = ["C", "D", "E", "F", "G", "H"];
+        tableauJeux[6] = ["C", "D", "E", "F", "G", "H", "I"];
+        tableauJeux[7] = ["E", "F", "G", "H"];
+        tableauJeux[8] = ["G"];
+
+        return (tableauJeux[l - 1].indexOf(c) !== -1);
+
+    }
 };
--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@
 
 var LyngkTestCase = TestCase("LyngkTestCase");
 
-LyngkTestCase.prototype.testStory1=function(){
-    var coordinates=new Lyngk.Coordinates(A,1);
+LyngkTestCase.prototype.testStory1 = function () {
+    var coordinates = new Lyngk.Coordinates("A", 1);
 
     assertFalse(coordinates.is_valid());
 };
+
