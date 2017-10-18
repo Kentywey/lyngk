7bf6d176d5a8fa15b996aa983613762036fc48d9
Kentywey <kweymeesch@gmail.com>
2017-10-18 11:15:14 +0200

    Ajout du Actual state

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,6 +2,16 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function () {
+    var etatActuel;
 
+    this.init = function () {
+        etatActuel = Lyngk.State.VACANT;
+    };
+
+    this.init();
+
+    this.get_etatActuel = function () {
+        return etatActuel;
+    };
 };
