cd831e06ad393fe2df2b32f3fca0fae7f9efda5d
Kentywey <kweymeesch@gmail.com>
2017-10-18 13:40:39 +0200

    Fixe Typo + Debut exo 11

--- src/Engine.js
+++ src/Engine.js
@@ -1,8 +1,6 @@
 "use strict";
 
 // enums definition
-Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
-
-Lyngk.Engine = function () {
-
-};
+Lyngk.Couleur = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
+Lyngk.Lignes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Lyngk.Colonnes = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
